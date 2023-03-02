ea8b1f302ef168f0a007b6e1c198b06bc77f4157
vandananayak <62019858+vandananayak@users.noreply.github.com>
2023-03-02 21:09:15 +0530

    O(n^2) complexity example

--- box.ts
+++ box.ts
@@ -1,9 +1,14 @@
 export class Box{
+  pair = [];
   public box(){
     const boxes = [1,2,3,4];
-    boxes.forEach((box)=>{
-      let pair = [];
-      
-    })
-  }
+    boxes.forEach((box,i)=>{
+      boxes.forEach((box,j)=>{
+        this.pair.push([boxes[i],boxes[j]]);
+      })
+    });
+
+    console.log(this.pair)
+  }    
 }
+// O(n^2) complexity
